3a17895f5dbdbd1dd47e94186488bbe7ec4c35c1
tedy.saputro <tedy@saputro.dev>
2025-02-08 09:00:36 +0700

    add maps

--- src/main/java/com/subrutin/game/legendofjava/tile/TileManager.java
+++ src/main/java/com/subrutin/game/legendofjava/tile/TileManager.java
@@ -1,6 +1,9 @@
 package com.subrutin.game.legendofjava.tile;
 
 import java.awt.Graphics2D;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
 
@@ -10,17 +13,59 @@ public class TileManager {
 
     GamePanel gp;
     Tile[] tiles;
-    
+    int mapTileNum[][];
+
     public TileManager(GamePanel gp) {
         this.gp = gp;
         tiles = new Tile[10];
+        mapTileNum = new int[gp.MAX_SCREEN_COL][gp.MAX_SCREEN_ROW];
         loadTiles();
+        buildMap();
     }
     
     public void loadTiles() {
         try {
             tiles[0] = new Tile();
-            tiles[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/002.png"));            
+            tiles[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/002.png"));
+            tiles[0].collision = true;
+            
+            tiles[1] = new Tile();
+            tiles[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/032.png"));
+            tiles[1].collision = true;
+
+            tiles[2] = new Tile();
+            tiles[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/004.png"));
+            tiles[2].collision = true;
+            
+                        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void buildMap() {
+        //load from src/main/resources/map/map01.text
+        String line = "";
+        try {
+            InputStream is = getClass().getResourceAsStream("/map/map01.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            int col = 0;
+            int row = 0;
+            while (col < gp.MAX_SCREEN_COL && row < gp.MAX_SCREEN_ROW) {
+                line = br.readLine();
+                String[] tokens = line.split(" ");
+                for (int i = 0; i < tokens.length; i++) {
+                    mapTileNum[col][row] = Integer.parseInt(tokens[i]);
+                    col++;
+                }
+                if(col == gp.MAX_SCREEN_COL) {
+                    col = 0;
+                    row++;
+                } 
+
+            }
+            br.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -32,7 +77,9 @@ public class TileManager {
         int x = 0;
         int y = 0;
         while (col < gp.MAX_SCREEN_COL && row < gp.MAX_SCREEN_ROW) {
-            g2.drawImage(tiles[0].image, x, y, gp.TILE_SIZE, gp.TILE_SIZE, null);
+            int tileNum = mapTileNum[col][row];
+            g2.drawImage(tiles[tileNum].image, x, y, gp.TILE_SIZE, gp.TILE_SIZE, null);
+
             col++;
             x += gp.TILE_SIZE;
             if (col == gp.MAX_SCREEN_COL) {
